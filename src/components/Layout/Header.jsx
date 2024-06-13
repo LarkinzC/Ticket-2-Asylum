ce1360269b9b06af80db29368bd7c26629c842c8
Larkin Campbell <Larkincampbell24@gmail.com>
2024-06-13 16:42:01 -0700

    t3 progres

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,10 +3,17 @@ import { Image } from 'antd';
 import { Link } from 'react-router-dom';
 import Logo from '../../styles/Images/WhiteLogo.png';
 import { colors } from '../../styles/data_vis_colors';
+import LogoutButton from '../common/LogoutButton';
+import LoginButton from '../common/LoginButton';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Nav } from 'react-bootstrap';
+
 
 const { primary_accent_color } = colors;
 
 function HeaderContent() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div
       style={{
@@ -21,13 +28,16 @@ function HeaderContent() {
           <Image width={100} src={Logo} preview={false} alt="HRF logo white" />
         </a>
       </div>
-      <div>
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
         <Link to="/" style={{ color: '#E2F0F7', paddingRight: '75px' }}>
           Home
         </Link>
         <Link to="/graphs" style={{ color: '#E2F0F7' }}>
           Graphs
         </Link>
+        <Nav >
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        </Nav>
       </div>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -22,6 +22,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './state/reducers';
 import { colors } from './styles/data_vis_colors';
+import Auth0ProviderWithHistory from './components/auth0-provider-with-history';
 
 const { primary_accent_color } = colors;
 
@@ -30,7 +31,9 @@ ReactDOM.render(
   <Router>
     <Provider store={store}>
       <React.StrictMode>
-        <App />
+      <Auth0ProviderWithHistory>
+        <App /> 
+        </Auth0ProviderWithHistory>
       </React.StrictMode>
     </Provider>
   </Router>,
